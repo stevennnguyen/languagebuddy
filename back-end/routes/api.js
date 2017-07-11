2ac0d14a483ad71380954c6fddf080302d94055c
stevennnguyen <steven.nguyen194@gmail.com>
2017-07-11 13:31:48 -0700

    api call for all language events in a certain area shown on the map

--- back-end/routes/api.js
+++ back-end/routes/api.js
@@ -4,7 +4,7 @@ const request = require('request');
 
 //get a list of meetup events
 router.get('/events', function(req, res, next){
-    request('https://api.meetup.com/Orange-County-American-Japanese-Language-and-Friendship/events?photo-host=public&page=2&sig_id=203258650&sig=411f991b03f4b41ff91f89cc0193bef3a9ef5e18', function (error, response, body) {
+    request('https://api.meetup.com/2/events?member_id=203258650&offset=0&format=json&limited_events=False&photo-host=public&page=20&fields=&order=time&desc=false&status=upcoming&sig_id=203258650&sig=fae016a4c1fe7bc1a64a269b54d1daf3474c8de0', function (error, response, body) {
     if (!error && response.statusCode == 200) {
         //console.log(body);
         res.send(body);
@@ -12,4 +12,5 @@ router.get('/events', function(req, res, next){
     })
 });
 
+//https://api.meetup.com/Orange-County-American-Japanese-Language-and-Friendship/events?photo-host=public&page=2&sig_id=203258650&sig=411f991b03f4b41ff91f89cc0193bef3a9ef5e18
 module.exports = router;
--- front-end/src/app/meetup-map/meetup-map.component.ts
+++ front-end/src/app/meetup-map/meetup-map.component.ts
@@ -11,17 +11,15 @@ declare var mapProp: any;
 })
 export class MeetupMapComponent implements OnInit {
   title: string = 'My first AGM project';
-  lat: number = 33.68535614013672;
-  lng: number = -117.81024932861328;
-  lat2: number = 33.842350006103516;
-  lng2: number = -118.38854217529297;
+  homeLat: number = 33.68535614013672;
+  homeLng: number = -117.81024932861328;
   events = {};
 
   constructor(private eventsService: EventsService) { }
 
   ngOnInit() {
     this.eventsService.fetchEvents().subscribe(
-      (events) => this.events = events
+      (events) => this.events = events.results
     )
   }
 
