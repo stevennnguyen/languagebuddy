e1b48365cbbb9a5d97b4e964017438faa5aaf9db
stevennnguyen <steven.nguyen194@gmail.com>
2017-07-12 11:17:21 -0700

    setup basic routing

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,25 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MeetupMapComponent } from './meetup-map/meetup-map.component';
+import { ResourcesComponent } from './resources/resources.component';
 
 import { HttpModule } from '@angular/http'; 
 import { AgmCoreModule } from '@agm/core';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
 
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: MeetupMapComponent },
+  { path: 'resources', component: ResourcesComponent },
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     MeetupMapComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    ResourcesComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCz6oDVWAp1kgxS8MPcZpkLSvfcK9H3B_Y'
-    })
+    }),
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
